d5bda04bcc9c84da7868e983961f9ac918e58eed
Sebastián Hernández <sebastian.her0548@gmail.com>
2023-12-16 22:01:46 -0300

    Eliminando una clase

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -2,8 +2,8 @@
 export default function Alert({error,success}){
     return(
         <div className="alert_msg">
-            {error.length>0 &&  <h3 className="msg_error" style={{ color: "red" }}>{error}</h3>}
-            {success.length>0 &&  <h3 className="msg_success" style={{ color: "green"}}>{success}</h3>}
+            {error.length>0 &&  <h3 className="msg_error">{error}</h3>}
+            {success.length>0 &&  <h3 className="msg_success">{success}</h3>}
             
         </div>
 
