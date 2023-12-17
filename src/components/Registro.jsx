9ccc061dcbbfd7d3cfb0e40f4972a6eec538b990
CynthiaZuniga <cynthia.zuniga.a@gmail.com>
2023-12-17 12:26:48 -0300

    icons

--- src/components/Registro.jsx
+++ src/components/Registro.jsx
@@ -8,9 +8,9 @@ export default function Registro({error, setSuccess, setError, success}) {
         <div className="layout__registro">
             <h1 className="registro__h1">Crea una cuenta</h1>
             <div className="registro__icons">
-                <SocialButton icon='fa-brands fa-facebook fa-3x icon '></SocialButton>
-                <SocialButton icon='fa-brands fa-github fa-3x icon '></SocialButton>
-                <SocialButton icon='fa-brands fa-linkedin fa-3x icon '></SocialButton>
+                <SocialButton icon='fa-brands fa-facebook fa-2x icon '></SocialButton>
+                <SocialButton icon='fa-brands fa-github fa-2x icon '></SocialButton>
+                <SocialButton icon='fa-brands fa-linkedin fa-2x icon '></SocialButton>
             </div>
             <h5 className="registro__h5">O usa tu email para registrarte</h5>
             <Formulario error={error} setSuccess={setSuccess} setError={setError} />
